03cbe1ab09223ad1e337a0753b32720e92001c4a
David Mortkowitz and Regina Nurieva <david@>
2018-08-06 13:59:20 -0700

    Add Javascript to describe simple mathematics for addition, subtraction, multiplication and division.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
+
+
 var add = function(number1, number2){
 	return number1 + number2;
 };
+var subtract = function(number1, number2){
+	return number1 - number2;
+};
+var divide = function(number1, number2){
+	return number1 / number2;
+};
+var multiply = function(number1, number2){
+	return number1 * number2;
+};
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
 
-alert(add(number1, number2));
+
+var number1 = parseInt(prompt("Add a number:"));
+var number2 = parseInt(prompt("Add another number:"));
+var result = add(number1, number2);
+var result1 = subtract(number1, number2);
+var result3 = divide(number1, number2);
+var result4 = multiply(number1, number2);
+alert(result);
+alert(result1);
+alert(result3);
+alert(result4);
