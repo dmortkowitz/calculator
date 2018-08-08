70dcc75ac5756800f90ef8045a79fe2ea8330f27
David Mortkowitz and Hyewon Cho <hyewon@>
2018-08-08 14:34:53 -0700

    Add javascript and HTML to convert gallons to liters, and vice versa.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,24 @@
 
 
-var add = function(number1, number2){
-	return number1 + number2;
+var GToL = function(number1){
+	return number1 * 3.78541;
 };
-var subtract = function(number1, number2){
-	return number1 - number2;
+var LToG = function(number2){
+	return number2 / 3.78541;
 };
-var divide = function(number1, number2){
-	return number1 / number2;
-};
-var multiply = function(number1, number2){
-	return number1 * number2;
-};
+
+$(document).ready(function() {
+	$("#gallonConvert").submit(function(event){
+		var inputGallon = parseInt($("input#gallon").val());
+		var outputLiter = GToL(inputGallon).toFixed(2);
+		$("#outputLiter").text(outputLiter);
+		event.preventDefault();
+	});
+
+	$("#literConvert").submit(function(event){
+		var inputLiter = parseInt($("input#liter").val());
+		var outputGallon = LToG(inputLiter).toFixed(2);
+		$("#outputGallon").text(outputGallon);
+		event.preventDefault();
+	});
+});
