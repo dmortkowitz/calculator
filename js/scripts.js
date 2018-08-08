471b9af096535ba2e50ac83031b5a41d4801575f
David Mortkowitz and Hyewon Cho <hyewon@>
2018-08-08 10:51:13 -0700

    Add addition function which collects input from user form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-
-
 var add = function(number1, number2){
 	return number1 + number2;
 };
@@ -13,15 +11,16 @@ var multiply = function(number1, number2){
 	return number1 * number2;
 };
 
+$(document).ready(function(){
+	// var result1 = subtract(number1, number2);
+	// var result3 = divide(number1, number2);
+	// var result4 = multiply(number1, number2);
+	$("form#input").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		$("span#result").text(result);
+	});
 
-
-var number1 = parseInt(prompt("Add a number:"));
-var number2 = parseInt(prompt("Add another number:"));
-var result = add(number1, number2);
-var result1 = subtract(number1, number2);
-var result3 = divide(number1, number2);
-var result4 = multiply(number1, number2);
-alert(result);
-alert(result1);
-alert(result3);
-alert(result4);
+});
