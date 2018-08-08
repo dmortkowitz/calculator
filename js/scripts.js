885f563fc05c67cd519e59cb9e76833be34c9a98
David Mortkowitz and Hyewon Cho <david@>
2018-08-08 14:03:15 -0700

    Add HTML for forms for calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -12,16 +12,3 @@ var divide = function(number1, number2){
 var multiply = function(number1, number2){
 	return number1 * number2;
 };
-
-
-
-var number1 = parseInt(prompt("Add a number:"));
-var number2 = parseInt(prompt("Add another number:"));
-var result = add(number1, number2);
-var result1 = subtract(number1, number2);
-var result3 = divide(number1, number2);
-var result4 = multiply(number1, number2);
-alert(result);
-alert(result1);
-alert(result3);
-alert(result4);
