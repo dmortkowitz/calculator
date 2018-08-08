114f70d9369e0f760dd5e6e01cdc6c47ce6aa9ab
David Mortkowitz and Hyewon Cho <david@>
2018-08-08 11:05:07 -0700

    Add subtraction, multiplication, and division functions to calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -12,15 +12,33 @@ var multiply = function(number1, number2){
 };
 
 $(document).ready(function(){
-	// var result1 = subtract(number1, number2);
-	// var result3 = divide(number1, number2);
-	// var result4 = multiply(number1, number2);
-	$("form#input").submit(function(event){
+	$("form#inputAdd").submit(function(event){
 		event.preventDefault();
 		var number1 = parseInt($("#add1").val());
 		var number2 = parseInt($("#add2").val());
 		var result = add(number1, number2);
-		$("span#result").text(result);
+		$("span#resultAdd").text(result);
+	});
+	$("form#inputSub").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#sub1").val());
+		var number2 = parseInt($("#sub2").val());
+		var result = subtract(number1, number2);
+		$("span#resultSub").text(result);
+	});
+	$("form#inputMult").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#mult1").val());
+		var number2 = parseInt($("#mult2").val());
+		var result = multiply(number1, number2);
+		$("span#resultMult").text(result);
+	});
+	$("form#inputDiv").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#div1").val());
+		var number2 = parseInt($("#div2").val());
+		var result = divide(number1, number2);
+		$("span#resultDiv").text(result);
 	});
 
 });
